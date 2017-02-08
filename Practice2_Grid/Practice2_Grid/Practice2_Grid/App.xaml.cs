ad62f97ddffc4c0e414688f25aaf792f20187e4c
ytabuchi @ Surface <ytabuchi.xlsoft@gmail.com>
2017-02-08 18:34:47 +0900

    add practice 1 to 3
    Learning about layout

---
+++ Practice2_Grid/Practice2_Grid/Practice2_Grid/App.xaml.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Practice2_Grid
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            // The root page of your application
+            MainPage = new NavigationPage(new HomePage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Practice2_Grid/Practice2_Grid/Practice2_Grid/HomePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Practice2_Grid
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
